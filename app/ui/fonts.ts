f2d02533655be3c17bff28d2bf663dea5b302bd4
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-10-09 20:28:24 -0300

    feat: setup-project-base

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lobster, Quicksand, Comfortaa } from 'next/font/google';
+
+export const lobster = Lobster({ weight: ['400'], subsets: ['latin'] });
+
+export const quicksand = Quicksand({ weight: ['700'], subsets: ['latin'] });
+
+export const comfortaa = Comfortaa({ weight: ['700'], subsets: ['latin'] });
