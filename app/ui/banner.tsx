f9baf351faf0b945adb954260dce10777a7c81f5
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-10-11 22:22:45 -0300

    feat: create components `header` `banner` `cards`

---
+++ app/ui/banner.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import { quicksand } from '@/app/ui/fonts';
+
+interface BannerProps {
+  title: string;
+  subtitle: string;
+  src: string;
+}
+
+export default function Banner({ src, title, subtitle }: BannerProps) {
+  return (
+    <div className="relative w-full h-96">
+      <Image
+        src={src}
+        alt="Banner"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-[-1] rounded-xl"
+      />
+      <div className="absolute inset-0 flex flex-col items-center sm:items-start justify-center text-center p-4">
+        <h1 className={`text-white ${quicksand.className} text-4xl  mb-0`}>
+          {title}
+        </h1>
+        <p className={`text-white ${quicksand.className} text-lg text-center`}>
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ app/ui/header.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { lobster } from '@/app/ui/fonts';
+
+export default function Header() {
+  return (
+    <header className="flex justify-between md:grid md:grid-cols-3 p-4 relative max-h-52">
+      <div className="flex justify-start items-end">
+          <Image
+            src="/symbol.svg"
+            width={172}
+            height={91}
+            className="hidden lg:block"
+            alt="Simbolo do meu amor"
+          />
+      </div>
+      <div className="flex justify-center items-center">
+        <span className="flex">
+          <h1 className={`text-blue-500 ${lobster.className} md:text-10xl`}>Eter</h1>
+          <h1 className={`${lobster.className} md:text-10xl`}>nizze</h1>
+        </span>
+      </div>
+      <div className="flex justify-end items-center">
+        <Link
+          href="/login"
+          className="items-center col-span-2 gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        >
+          <span>Log in</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
