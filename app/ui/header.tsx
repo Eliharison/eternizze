e5b8cd948c6a7ab9edb27d9d554a9874b40f43e8
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-11-28 15:52:00 -0300

    HotFix: Logo-Center

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -3,7 +3,7 @@ import Logo from '@/app/ui/logo';
 
 export default async function Header() {
   return (
-    <header className="flex justify-between md:grid md:grid-cols-3 p-4 relative max-h-52">
+    <header className="flex justify-center md:grid md:grid-cols-3 p-4 relative max-h-52">
       <div className="flex justify-start items-end">
         <Image
           src="/symbol.svg"
@@ -13,7 +13,7 @@ export default async function Header() {
           alt="Simbolo do meu amor"
         />
       </div>
-      <div className="grid columns-2 justify-center items-center">
+      <div className="lg:grid lg:columns-2 justify-center items-center">
         <Logo width="10xl" />
       </div>
     </header>
